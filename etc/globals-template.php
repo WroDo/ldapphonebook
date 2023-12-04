f8ff06e7bcbaac340e37c29ebad0eac1fc566c8f
Heiko <heiko@HeikosL15.fritz.box>
2023-12-04 18:23:12 +0100

    Foo

--- etc/globals-template.php
+++ etc/globals-template.php
@@ -6,11 +6,27 @@ $gFileLog           =   "$gRootPath/log/#log.log"; /* # Makes CVS ignore the fil
 $gLogLevel          =   2;              /* As usual: 0 = only errors, 1 = warnings, 2 = everything, 3 = even more (floods the log :) ) */
 $gLocaleWeb         =   "en_US.utf8";   /* See your LAMP's locale -a */
 $gFileLogMaxSize    =   1024*1024*42;   /* 42MB :) */ 
-$gMaxFilenameLength =	64;
 $gSiteLogo			=	"images/companylogo.png";
 $gCustomHeaderLines	=	"";
-$gSiteLanguage		=	"de"; 
+$gSiteLanguage		=	"de"; // switch here to en or make it dependant on browser's language
+//$basedn=array('dmdName=users,dc=foo,dc=fr','dmdName=users,dc=bar,dc=com');  // two basedn
+$gLdapBaseDn		=	'TBD';
+$gLdapFilter		=	'(&(objectClass=inetOrgPerson)(uid=*))';  // single filter
+$gLdapAttributes	=	array('dn','uid','sn');
+$gLdapBindUser		=	", , ";
+$gLdapBindPassword	=	'something cryptic';
+$gLdapServerHost	=	'buead1.intern.hcsn.de';
+$gLdapServerPort	=	389;
+
+$gLdapConnections	=	array(
+	0	=>	array(
+		'server'=>'buead1.intern.hcsn.de', 'port'=>389, 'user'=>'ldap.blah', 'password'=>'SECRET', 'basedn'='TBD', 'filter'='(&(objectClass=inetOrgPerson)(uid=*))', 'attributes'=>array('dn','uid','sn')
+				 ),
+	# Add more connections if you have multiple directories...
+);
 
 
+# MAKE MULTIPLE SERVERS POSSIBLE!
+# COPY TO TEMPLATE!
 
 ?>
