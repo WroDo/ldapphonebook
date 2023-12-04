b76f583a8e8ab067a0ae44efed5ecabde318e16b
Heiko <heiko@HeikosL15.fritz.box>
2023-12-04 14:50:05 +0100

    fix

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 echo("
 	<hr>
 	<font size=\"1\">
-		<center>$gSiteName - <a href=\"https://github.com/WroDo/pdfconvertomat\">Project Page</a></center>
+		<center>$gSiteName - <a href=\"https://github.com/WroDo/ldapphonebook\">Project Page</a></center>
 	</font>
 	</body>
 </html>
