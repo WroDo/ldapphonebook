248a3699e53fd49397743f997a037f3808caeaca
Heiko <heiko@HeikosL15.fritz.box>
2023-12-06 09:36:23 +0100

    Walk array in sandbox

--- sandbox.php
+++ sandbox.php
@@ -1,9 +1,9 @@
 #!/usr/bin/php
 <?php
-set_time_limit(30);
+/*set_time_limit(30);
 error_reporting(E_ALL);
 ini_set('error_reporting', E_ALL);
-ini_set('display_errors',1);
+ini_set('display_errors',1);*/
 
 /* Includes */
 require_once('etc/globals.php');
@@ -12,40 +12,55 @@ require_once('include/commonLogging.php');
 require_once('include/commonFiles.php');
 require_once('include/internationalization.php');
 
-/* (Un)main */
-if ($lLdapConnection = ldap_connect($gLdapServerHost, $gLdapServerPort)) // does not actually connect, bind does!
+/* Get the connections and walk thru them */
+for ($gLdapConnections as $gLdapConnectionNumber => $gLdapConnectionArray)
 {
-	ldap_set_option($lLdapConnection, LDAP_OPT_PROTOCOL_VERSION, 3);
-	ldap_set_option($lLdapConnection, LDAP_OPT_NETWORK_TIMEOUT, 5); // default timeout is about 2 minutes, setting it to five seconds
-
-	if (ldap_bind($lLdapConnection, $gLdapBindUser, $gLdapBindPassword)) // bind
+	$lLdapServer=$gLdapConnectionArray['server'];
+	$lLdapPort=$gLdapConnectionArray['port'];
+	$lLdapUser=$gLdapConnectionArray['user'];
+	$lLdapPassword=$gLdapConnectionArray['password'];
+	$lLdapBaseDn=$gLdapConnectionArray['basedn'];
+	$lLdapFilter=$gLdapConnectionArray['filter'];
+	$lLdapAttributes=$gLdapConnectionArray['attributes'];
+	
+	/* (Un)main */
+	if ($lLdapConnection = ldap_connect($lLdapServer, $lLdapPort)) // does not actually connect, bind does!
 	{
-		if($lLdapSearch = ldap_search($lLdapConnection, $gLdapBaseDn, $gLdapFilter, $gLdapAttributes)) // initiate search
+		ldap_set_option($lLdapConnection, LDAP_OPT_PROTOCOL_VERSION, 3);
+		ldap_set_option($lLdapConnection, LDAP_OPT_NETWORK_TIMEOUT, $gLdapTimeout); // default timeout is about 2 minutes, setting it to five seconds
+
+		if (ldap_bind($lLdapConnection, $lLdapUser, $lLdapPassword)) // bind
 		{
-			for($i=0; $i<count($lLdapSearch); $i++) // walk search results
+			if($lLdapSearch = ldap_search($lLdapConnection, $lLdapBaseDn, $lLdapFilter, $lLdapAttributes)) // initiate search
 			{
-				$lLdapSearchResult=ldap_get_entries($lLdapConnection, $lLdapSearch[$i]);
-				print_r($lLdapSearchResult);
-				print "\n";
-				say("lLdapSearchResult: ", __FILE__, __FUNCTION__, __LINE__, 2);
-				sayArray($lLdapSearchResult, __FILE__, __FUNCTION__, __LINE__, 2);
+				for($i=0; $i<count($lLdapSearch); $i++) // walk search results
+				{
+					$lLdapSearchResult=ldap_get_entries($lLdapConnection, $lLdapSearch[$i]);
+					print_r($lLdapSearchResult);
+					print "\n";
+					say("lLdapSearchResult: ", __FILE__, __FUNCTION__, __LINE__, 2);
+					sayArray($lLdapSearchResult, __FILE__, __FUNCTION__, __LINE__, 2);
+				} // walk search results
 			}
+			else
+			{
+				say("LDAP search failed (gLdapBaseDn: $gLdapBaseDn, gLdapFilter: $gLdapFilter, gLdapAttributes: $gLdapAttributes)", __FILE__, __FUNCTION__, __LINE__, 0);
+			} // search
 		}
 		else
 		{
-			say("LDAP search failed (gLdapBaseDn: $gLdapBaseDn, gLdapFilter: $gLdapFilter, gLdapAttributes: $gLdapAttributes)", __FILE__, __FUNCTION__, __LINE__, 0);
-		}
+			say("LDAP bind for user \"$lLdapUser\" failed.", __FILE__, __FUNCTION__, __LINE__, 0);
+		} // bind
+		ldap_close($lLdapConnection);
 	}
 	else
 	{
-		say("LDAP bind for user \"$gLdapBindUser\" failed.", __FILE__, __FUNCTION__, __LINE__, 0);
-	}
-	ldap_close($lLdapConnection);
-}
-else
-{
-	say("Connecting \"$gLdapServerHost:$gLdapServerPort\" failed.", __FILE__, __FUNCTION__, __LINE__, 0);
-}
+		say("Connecting \"$lLdapServer:$lLdapPort\" failed.", __FILE__, __FUNCTION__, __LINE__, 0);
+	} // connect
+
+} // for connection
+
+
 
 
 
